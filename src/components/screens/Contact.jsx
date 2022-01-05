2146fc39c356dc1ed4c38b0b71f0db5037224bd2
Facundo <facuunion13@gmail.com>
2022-01-05 03:26:08 -0300

    comento por las dudas

--- src/components/screens/Contact.jsx
+++ src/components/screens/Contact.jsx
@@ -8,8 +8,8 @@ export const Contact = () => {
                 {/*  <h1>Contact</h1> */}
                 <a  href="https://www.linkedin.com/in/facundo-perez-agustin/" target="_blank" rel="noopener noreferrer"> <i className="fab fa-linkedin"></i></a>
                 <a  href="https://github.com/facu1995" target="_blank" rel="noopener noreferrer"> <i className="fab fa-github"></i></a>
-                <a href="mailto:facuunion13@gmail.com?Subject=Consulta" > <i className="fas fa-envelope"></i></a>
-                <a  href="https://api.whatsapp.com/send?phone=542235290769&text=" target="_blank" rel="noopener noreferrer"><i className="whatsapp fab fa-whatsapp"></i></a>
+               {/*  <a href="mailto:facuunion13@gmail.com?Subject=Consulta" > <i className="fas fa-envelope"></i></a>
+                <a  href="https://api.whatsapp.com/send?phone=542235290769&text=" target="_blank" rel="noopener noreferrer"><i className="whatsapp fab fa-whatsapp"></i></a> */}
                 {/* <a  href="" target="_blank"><i className="fas fa-user-circle"></i></a> */}
 
 
