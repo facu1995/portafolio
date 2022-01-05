c0ef7b118c727c5388a51b2a8018815000d3d765
Facundo <facuunion13@gmail.com>
2022-01-05 03:21:47 -0300

    _blank rel="noopener noreferrer"

--- src/components/screens/Contact.jsx
+++ src/components/screens/Contact.jsx
@@ -8,8 +8,8 @@ export const Contact = () => {
                 {/*  <h1>Contact</h1> */}
                 <a className="" href="https://www.linkedin.com/in/facundo-perez-agustin/" target="_blank" rel="noopener noreferrer"> <i className="fab fa-linkedin"></i></a>
                 <a className="" href="https://github.com/facu1995" target="_blank" rel="noopener noreferrer"> <i className="fab fa-github"></i></a>
-                <a href="mailto:facuunion13@gmail.com?Subject=Consulta"  rel="noopener noreferrer"> <i className="fas fa-envelope"></i></a>
-                <a target="_blank" href="https://api.whatsapp.com/send?phone=542235290769&text="  rel="noopener noreferrer"><i className="whatsapp fab fa-whatsapp"></i></a>
+                <a href="mailto:facuunion13@gmail.com?Subject=Consulta" > <i className="fas fa-envelope"></i></a>
+                <a  href="https://api.whatsapp.com/send?phone=542235290769&text=" target="_blank" rel="noopener noreferrer"><i className="whatsapp fab fa-whatsapp"></i></a>
                 {/* <a className="" href="" target="_blank"><i className="fas fa-user-circle"></i></a> */}
 
 
